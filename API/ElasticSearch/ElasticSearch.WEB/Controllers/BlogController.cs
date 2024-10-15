b26a918ac23398d5b492b2ac6436e28092fb9ede
gül yılmaz <rose_-_1993@hotmail.com>
2024-10-15 15:07:37 +0300

    Example App(Blog App)

---
+++ API/ElasticSearch/ElasticSearch.WEB/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using ElasticSearch.WEB.Models;
+using ElasticSearch.WEB.Services;
+using ElasticSearch.WEB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticSearch.WEB.Controllers
+{
+    public class BlogController : Controller
+    { private  BlogService _blogService;
+
+        public BlogController(BlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(BlogCreateViewModel model)
+        {
+          var isSuccess=  await _blogService.SaveAsync(model);
+            if(!isSuccess)
+            {
+                TempData["result"] = "Kayıt Başarısız";
+                return RedirectToAction("Save");
+            }
+            TempData["result"] = "Kayıt Başarılı";
+            return RedirectToAction("Save");
+        }
+        public async Task< IActionResult> Search()
+        {
+           
+            return View(await _blogService.SearchAsync(string.Empty));
+        }
+        [HttpPost]
+        public async Task< IActionResult> Search(string searchText)
+        {
+            ViewBag.searchText = searchText;
+            var blogList=await _blogService.SearchAsync(searchText);
+            return View(blogList);
+        }
+    }
+}
---
+++ API/ElasticSearch/ElasticSearch.WEB/Repositories/BlogRepository.cs
@@ -0,0 +1,57 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.Nodes;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+using ElasticSearch.WEB.Models;
+using System.Collections.Immutable;
+
+namespace ElasticSearch.WEB.Repositories
+{
+    public class BlogRepository
+    {
+        private readonly ElasticsearchClient _elasticsearchClient;
+        private const string indexName = "blog";
+        public BlogRepository(ElasticsearchClient elasticsearchClient)
+        {
+            _elasticsearchClient = elasticsearchClient;
+        }
+        public async Task<Blog?> SaveAsync(Blog newBlog)
+        {
+            newBlog.Created = DateTime.Now;
+            var response = await _elasticsearchClient.IndexAsync(newBlog, x => x.Index(indexName));
+
+            if (!response.IsValidResponse) return null;
+            newBlog.Id = response.Id;
+            return newBlog;
+        }
+        public async Task<List<Blog>> SearchAsync(string searchText)
+        { List<Action<QueryDescriptor<Blog>>> ListQuery = new();
+
+            Action<QueryDescriptor<Blog>> matchAll = (q) => q.MatchAll(new MatchAllQuery());
+            Action<QueryDescriptor<Blog>> matchContent = (q) => q.Match(m => m
+                            .Field(f => f.Content)
+                             .Query(searchText));
+            Action<QueryDescriptor<Blog>> titleMatchBoolPrefix = (q) => q.MatchBoolPrefix(m => m
+                       .Field(f => f.Content)
+                        .Query(searchText));
+            Action<QueryDescriptor<Blog>> tagTerm = (q) => q.Term(t => t.Field(f => f.Tags).Value(searchText));
+            if (string.IsNullOrEmpty(searchText))
+            {
+                ListQuery.Add(matchAll);
+            }
+            else
+            {
+                ListQuery.Add(matchContent);
+                ListQuery.Add(titleMatchBoolPrefix);
+                ListQuery.Add(tagTerm);
+            }
+            //should((term1 and term2)or term3) olarak algılar hepsi veya olsun istersen araya virgül koy
+            var result = await _elasticsearchClient.SearchAsync<Blog>(s => s.Index(indexName)
+              .Query(q => q
+                  .Bool(b => b
+                    .Should(ListQuery.ToArray()))));
+            foreach (var hit in result.Hits) hit.Source.Id = hit.Id;
+
+            return result.Documents.ToList();
+        }
+    }
+}
